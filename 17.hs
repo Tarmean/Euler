0c857ee5d0dba700450ec54244f2c13eb679f971
Tarmean <cyril-fahlenbock@hotmail.de>
2017-02-13 22:05:46 +0100

    Clean up exercise 17

--- 17.hs
+++ 17.hs
@@ -4,26 +4,33 @@ import Data.Char (digitToInt, isAlpha)
 main = print $ total [1..1000]
 total :: [Int] -> Int
 total = sum . map lengthEnglish
-  where lengthEnglish = length . filter isAlpha . inEnglish
+  where lengthEnglish = length . filter isAlpha . transform
 
 
 ones = (!!) ["" ,"one" ,"two" ,"three" ,"four" ,"five" ,"six" ,"seven" ,"eight" ,"nine",
              "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen",
              "seventeen", "eighteen", "nineteen"]
 tens = (!!) ["" ,"teen" ,"twenty" ,"thirty" ,"forty" ,"fifty" ,"sixty" ,"seventy" ,"eighty" ,"ninety"]
-magnitudes = (!!) ["" ," thousand" ," million" ," billion"]
+magnitudes = (!!) [" hundred", " thousand" ," million" ," billion"]
 
-inEnglish :: Int -> String
-inEnglish i
+transform i
+  | i < 0 = "minus " ++ transform (-i)
   | i == 0 = "zero"
   | i < 20 = ones i
-  | i < 100 = format tens 10 "-"
-  | i < 1000 = format ((++ " hundred") . ones) 100 " and "
-  | otherwise = format ((++ magnitudes m). inEnglish) r " and "
+  | i < 100 = format tens 10 "-" i
+  | otherwise = withMag i
+
+format l mag divider i
+  | m == 0 = l d
+  | otherwise = l d ++ divider ++ transform m
+  where (d, m) = i `divMod` mag
+
+withMag i = format lhs mask " and " i
   where
-    m = (`div` 3) . lenght . show
-    r = 10^(m*3)
-    format l mag divider
-      | m == 0 = l d
-      | otherwise = l d ++ divider ++ inEnglish m
-      where (d, m) = i `divMod` mag
+    lhs = (++magnitude) . transform
+    magnitude = magnitudes mag
+    mask = max 100 $ 10^(mag*3)
+    mag = pred $ go i
+      where
+        go 0 = 0
+        go i = 1 + go (i `div` 1000)
